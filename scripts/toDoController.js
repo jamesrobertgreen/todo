7ca19c02ea2c41d1bd922bd2afb425a29fb87dca
James Green <jrgreen@gmail.com>
2016-08-31 23:28:48 +0100

    Add local storage

--- scripts/toDoController.js
+++ scripts/toDoController.js
@@ -1,19 +1,32 @@
 app.controller('toDoController', ['$scope', function ($scope) {
-    $scope.toDoList = [];
-    
+    $scope.init = function () {
+        var tmpToDolist = localStorage.getItem("toDoList");
+        if (tmpToDolist === '' || tmpToDolist === undefined || tmpToDolist === null) {
+            $scope.toDoList = [];
+        }
+        else { 
+            $scope.toDoList = JSON.parse(tmpToDolist);
+        }
+    };
     $scope.addItem = function (newItem) {
         var newItem = {
             "Item": newItem
             , "Done": false
         };
         $scope.toDoList = $scope.toDoList.concat(newItem);
+        updateStorage();
     };
-    
     $scope.markDone = function (item) {
         item.Done = !item.Done;
+        updateStorage();
     };
-    
     $scope.remove = function ($index) {
         $scope.toDoList.splice($index, 1);
+        updateStorage();
     };
+    
+    var updateStorage = function(){
+      localStorage.setItem("toDoList", JSON.stringify($scope.toDoList));  
+    };
+    
 }]);
